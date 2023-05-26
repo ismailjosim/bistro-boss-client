70ce5cdc6d066578d32502b8dc2a2086301cc872
ISMAIL <ismailjosim99@gmail.com>
2023-05-26 20:32:37 +0600

    add chefs recommends section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,18 +5,19 @@ import Categories from '../components/Home/Categories';
 import CTA from '../components/Home/CTA';
 import Menu from '../components/Home/Menu';
 import Info from '../components/Home/Info';
+import ChefsChoice from '../components/Home/ChefsChoice';
 
 
 const Home = () => {
     return (
-        <>
+        <main>
             <Slider />
             <Categories />
             <Info />
             <Menu />
             <CTA />
-
-        </>
+            <ChefsChoice />
+        </main>
     );
 };
 
---
+++ src/components/Home/ChefsChoiceCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import image from '../../assets/shop/banner2.jpg'
+
+const ChefsChoiceCard = () => {
+    return (
+        <div className="card w-full rounded-none bg-base-100 shadow-md">
+            <figure><img src={ image } alt="Shoes" /></figure>
+            <div className="card-body text-center">
+                <h2 className="text-2xl font-semibold">Caeser Salad</h2>
+                <p className='text-base'>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+                <div className='mt-5'>
+                    <button className="btn btn-primary btn-outline rounded-md">Add to cart</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ChefsChoiceCard;
