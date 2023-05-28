7380f8e00f60f1fb336904f91d8026821f2ddb09
ISMAIL <ismailjosim99@gmail.com>
2023-05-29 00:00:45 +0600

    add tab items

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -3,14 +3,50 @@ import '../CSS/custom.css'
 import 'react-tabs/style/react-tabs.css';
 import PageHeading from '../utils/PageHeading';
 import bannerImg from '../assets/shop/banner2.jpg'
+import { useState } from 'react';
+import useMenu from '../Hooks/useMenu';
+import Loading from '../utils/Loading';
+import FetchError from '../utils/FetchError';
+import MenuCard from '../components/Home/MenuCard';
+import ShopCard from '../components/Shop/ShopCard';
 
 
 const Shop = () => {
+    const [menu, loading, error, progress] = useMenu();
+    const [tabIndex, setTabIndex] = useState(0);
+
+    if (loading) {
+        return <div className='flex justify-center items-center'>
+            <Loading progress={ progress } />
+        </div>
+    }
+
+    if (error) {
+        return <FetchError message={ error } />
+    }
+
+    if (!loading && menu.length === 0) {
+        return <FetchError message={ "No Data Found!" } />
+    }
+
+    // Sort all data and map them
+    const allFood = menu.slice(0, 3).map((item, index) => <ShopCard key={ index } item={ item } />)
+    const dessertItem = menu.filter(item => item.category === "dessert").map((item, index) => <ShopCard key={ index } item={ item } />)
+    const pizzaItem = menu.filter(item => item.category === "pizza").map((item, index) => <ShopCard key={ index } item={ item } />)
+    const saladItem = menu.filter(item => item.category === "salad").map((item, index) => <ShopCard key={ index } item={ item } />)
+    const soupItem = menu.filter(item => item.category === "soup").map((item, index) => <ShopCard key={ index } item={ item } />)
+
+
+
     return (
         <main>
             <PageHeading bannerImg={ bannerImg } primary={ "Our shop" } secondary={ "would you like to try a dish?" } />
             <div className=''>
-                <Tabs className="w-11/12 mx-auto mt-10">
+                <Tabs
+                    defaultIndex={ tabIndex }
+                    className="w-11/12 mx-auto mt-10"
+                    onSelect={ (e) => setTabIndex(e) }
+                >
                     <TabList className={ "tabs flex justify-center items-center" }>
                         <Tab className={ `tab tab-bordered text-lg font-semibold` }>All Food</Tab>
                         <Tab className={ "tab tab-bordered text-lg font-semibold" }>Desserts</Tab>
@@ -20,106 +56,20 @@ const Shop = () => {
                     </TabList>
 
                     <TabPanel className={ 'w-11/12 mx-auto' }>
-                        <div className=' grid lg:grid-cols-3 gap-5 my-10'>
-
-                            { [1, 2, 3, 4].map((item, index) => {
-                                return (<div key={ index } className="w-full shadow-md rounded-md">
-                                    <img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-                                    <div className="flex flex-col justify-between p-6 space-y-8">
-                                        <div className="space-y-2">
-                                            <h2 className="text-3xl font-semibold tracking-wide">Donec lectus leo</h2>
-                                            <p className="">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-                                        </div>
-                                        <button type="button" className="btn btn-primary text-white">Read more</button>
-                                    </div>
-                                </div>
-                                )
-                            })
-
-                            }
-                        </div>
+                        <div className=' grid lg:grid-cols-3 gap-5 my-10'>{ allFood }</div>
                     </TabPanel>
                     <TabPanel className={ 'w-11/12 mx-auto' }>
-                        <div className=' grid lg:grid-cols-3 gap-5 my-10'>
-
-                            { [1, 2, 3, 4].map((item, index) => {
-                                return (<div key={ index } className="w-full shadow-md rounded-md">
-                                    <img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-                                    <div className="flex flex-col justify-between p-6 space-y-8">
-                                        <div className="space-y-2">
-                                            <h2 className="text-3xl font-semibold tracking-wide">Donec lectus leo</h2>
-                                            <p className="">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-                                        </div>
-                                        <button type="button" className="btn btn-primary text-white">Read more</button>
-                                    </div>
-                                </div>
-                                )
-                            })
-
-                            }
-                        </div>
+                        <div className=' grid lg:grid-cols-3 gap-5 my-10'>{ dessertItem }</div>
                     </TabPanel>
                     <TabPanel className={ 'w-11/12 mx-auto' }>
-                        <div className=' grid lg:grid-cols-3 gap-5 my-10'>
-
-                            { [1, 2, 3, 4].map((item, index) => {
-                                return (<div key={ index } className="w-full shadow-md rounded-md">
-                                    <img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-                                    <div className="flex flex-col justify-between p-6 space-y-8">
-                                        <div className="space-y-2">
-                                            <h2 className="text-3xl font-semibold tracking-wide">Donec lectus leo</h2>
-                                            <p className="">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-                                        </div>
-                                        <button type="button" className="btn btn-primary text-white">Read more</button>
-                                    </div>
-                                </div>
-                                )
-                            })
-
-                            }
-                        </div>
+                        <div className=' grid lg:grid-cols-3 gap-5 my-10'>{ pizzaItem }</div>
                     </TabPanel>
                     <TabPanel className={ 'w-11/12 mx-auto' }>
-                        <div className=' grid lg:grid-cols-3 gap-5 my-10'>
-
-                            { [1, 2, 3, 4].map((item, index) => {
-                                return (<div key={ index } className="w-full shadow-md rounded-md">
-                                    <img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-                                    <div className="flex flex-col justify-between p-6 space-y-8">
-                                        <div className="space-y-2">
-                                            <h2 className="text-3xl font-semibold tracking-wide">Donec lectus leo</h2>
-                                            <p className="">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-                                        </div>
-                                        <button type="button" className="btn btn-primary text-white">Read more</button>
-                                    </div>
-                                </div>
-                                )
-                            })
-
-                            }
-                        </div>
+                        <div className=' grid lg:grid-cols-3 gap-5 my-10'>{ saladItem }</div>
                     </TabPanel>
                     <TabPanel className={ 'w-11/12 mx-auto' }>
-                        <div className=' grid lg:grid-cols-3 gap-5 my-10'>
-
-                            { [1, 2, 3, 4].map((item, index) => {
-                                return (<div key={ index } className="w-full shadow-md rounded-md">
-                                    <img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-                                    <div className="flex flex-col justify-between p-6 space-y-8">
-                                        <div className="space-y-2">
-                                            <h2 className="text-3xl font-semibold tracking-wide">Donec lectus leo</h2>
-                                            <p className="">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-                                        </div>
-                                        <button type="button" className="btn btn-primary text-white">Read more</button>
-                                    </div>
-                                </div>
-                                )
-                            })
-
-                            }
-                        </div>
+                        <div className=' grid lg:grid-cols-3 gap-5 my-10'>{ soupItem }</div>
                     </TabPanel>
-
                 </Tabs>
             </div>
         </main>
---
+++ src/components/Shop/ShopCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BsCurrencyDollar, BsFillHeartFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+
+const ShopCard = ({ item }) => {
+    const { category, image, name, price, recipe, _id } = item || {}
+    return (
+        <div className="relative block overflow-hidden group">
+            <button className="absolute hover:bg-secondary hover:text-white right-4 top-4 z-10 bg-white transition-all duration-500 px-3 py-2 rounded-md font-semibold uppercase text-primary">
+                <span>{ category }</span>
+            </button>
+            <img src={ image } alt="" className="object-cover w-full h-64 transition duration-500 group-hover:scale-105 sm:h-72" />
+            <div className="relative p-6 bg-white border border-gray-100">
+                <div className="flex justify-between">
+                    <span className="whitespace-nowrap bg-primary text-white px-3 py-1.5 text-xs font-medium">New</span>
+                    <span className="flex items-center justify-center rounded-full  text-base font-semibold bg-secondary text-white px-2" >
+                        <BsCurrencyDollar />
+                        <p className="whitespace-nowrap">{ price }</p>
+                    </span>
+                </div>
+                <h3 className="mt-4 text-lg font-medium text-gray-900">{ name }</h3>
+                <p className="mt-1.5 font-light">{ recipe.slice(0, 100) }...<span className='font-semibold text-primary'>Read More</span></p>
+                <form className="mt-4">
+                    <Link to={ `/product/${ _id }` } className="btn btn-primary w-full text-sm font-medium transition text-white rounded hover:scale-105"> Add to Cart</Link>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default ShopCard;
