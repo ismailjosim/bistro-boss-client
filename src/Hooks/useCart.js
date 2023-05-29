2d038ffc4848349e5812b24005da216ad2471711
ISMAIL <ismailjosim99@gmail.com>
2023-05-30 01:18:06 +0600

    add carts

--- src/Hooks/useCart.js
+++ src/Hooks/useCart.js
@@ -8,7 +8,8 @@ const useCart = () => {
     const { user } = useContext(AuthContext);
     const { isLoading, isError, data: cart = [], error, refetch } = useQuery(['carts', user?.email], {
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/carts?email=${ user?.email }`);
+            // const res = await fetch(`http://localhost:5000/carts?email=${ user?.email }`);
+            const res = await fetch(`https://bistro-boss-server-git-main-ismailjosim.vercel.app/carts?email=${ user?.email }`);
             return res.json();
         },
     });
