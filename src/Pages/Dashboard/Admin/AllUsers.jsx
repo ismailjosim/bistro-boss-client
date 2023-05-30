a056236d89ab62c62f587def96fc5ed4368e0cda
ISMAIL <ismailjosim99@gmail.com>
2023-05-31 02:35:54 +0600

    add user remove and update admin functionality

--- src/Pages/Dashboard/Admin/AllUsers.jsx
+++ src/Pages/Dashboard/Admin/AllUsers.jsx
@@ -1,25 +1,81 @@
 import React from 'react';
+import Swal from 'sweetalert2'
+
 import { BsFillTrash3Fill } from 'react-icons/bs';
 import { IoIosPeople } from 'react-icons/io';
-import { RiAdminFill } from 'react-icons/ri';
 import SectionHeading from '../../../utils/SectionHeading';
 import useUser from '../../../Hooks/useUser';
 
 const AllUsers = () => {
-    const { users } = useUser();
+    const { users, refetch } = useUser();
+
+    // handle Update Admin
+    const handleUpdate = user => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: `make ${ user.name } Admit 🥰`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: "Confirm"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/users/admin/${ user._id }`, {
+                    method: 'PATCH'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.acknowledged) {
+                            Swal.fire({
+                                position: 'center',
+                                icon: 'success',
+                                title: 'Make Admin Successfully 🎉🎉',
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                            refetch()
+                        }
+                    })
 
 
-    const handleUpdate = id => {
-        fetch(`http://localhost:5000/users/admin/${ id }`, {
-            method: 'PATCH',
+            }
         })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                if (data.modifiedCount) {
-                    alert("Make Admin Successfully!")
-                }
-            })
+    }
+
+    // Delete User From Database
+    const handleDelete = id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: `You want to Delete`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: "Confirm"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/users/${ id }`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.acknowledged) {
+
+                            Swal.fire({
+                                position: 'center',
+                                icon: 'success',
+                                title: 'User Deleted Successfully 🎉🎉',
+                                showConfirmButton: false,
+                                timer: 1000
+                            })
+                            refetch()
+                        }
+                    })
+
+
+            }
+        })
     }
 
 
@@ -42,23 +98,24 @@ const AllUsers = () => {
                     </thead>
                     <tbody>
 
-                        { users && users.map((item, index) => {
-                            const { _id, name, email, role } = item || {}
+                        { users && users.map((user, index) => {
+                            const { _id, name, email, role } = user || {}
                             return (
                                 <tr key={ index }>
                                     <th>{ index + 1 }</th>
                                     <td>{ name }</td>
                                     <td>{ email }</td>
                                     <td>
-                                        <button onClick={ () => handleUpdate(_id) } className='btn btn-secondary text-white rounded-md'>
-                                            { role && role === 'admin' ?
-                                                <RiAdminFill size={ 25 } /> :
+                                        { role && role ?
+                                            <span className='font-semibold uppercase'>Admin</span>
+                                            :
+                                            <button onClick={ () => handleUpdate(user) } className='btn btn-secondary text-white rounded-md'>
                                                 <IoIosPeople size={ 25 } />
-                                            }
-                                        </button>
+                                            </button>
+                                        }
                                     </td>
                                     <td>
-                                        <button className='btn btn-error text-white rounded-md'>
+                                        <button onClick={ () => handleDelete(_id) } className='btn btn-error text-white rounded-md'>
                                             <BsFillTrash3Fill size={ 20 } />
                                         </button>
                                     </td>
